a71548b9e2ae259accefba908dc15069bd82f5ac
Enzo Luo <duenzo1010@gmail.com>
2022-07-06 03:39:36 +0800

    sort.refactor: use abstract class

--- ztm/typescript/sort/src/index.ts
+++ ztm/typescript/sort/src/index.ts
@@ -1,18 +1,13 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 // const numberCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numberCollection);
-// sorter.sort();
-
+// numberCollection.sort();
 // console.log(numberCollection.data);
 
 // const charactersCollection = new CharactersCollection('Xaayb');
-// const sort = new Sorter(charactersCollection);
-// sort.sort();
-
+// charactersCollection.sort();
 // console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -21,6 +16,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();

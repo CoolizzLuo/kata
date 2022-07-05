d02623ca7b7c2bae8992f915e11aa9875763b855
Enzo Luo <duenzo1010@gmail.com>
2022-07-06 02:13:06 +0800

    sort.feat: implementation Linkedlist

--- ztm/typescript/sort/src/index.ts
+++ ztm/typescript/sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numberCollection = new NumbersCollection([10, 3, -5, 0]);
 // const sorter = new Sorter(numberCollection);
@@ -8,8 +9,18 @@ import { CharactersCollection } from './CharactersCollection';
 
 // console.log(numberCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sort = new Sorter(charactersCollection);
-sort.sort();
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sort = new Sorter(charactersCollection);
+// sort.sort();
 
-console.log(charactersCollection.data);
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();

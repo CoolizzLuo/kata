c16cc03aae6e8f636dfbbb8c6a641947b9069224
Enzo Luo <duenzo1010@gmail.com>
2022-07-09 17:17:53 +0800

    [typescript] feat: Ch149 done

--- typescript/web-framework/src/index.ts
+++ typescript/web-framework/src/index.ts
@@ -1,1 +1,8 @@
-console.log('hi there');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.set({ name: 'newname' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));

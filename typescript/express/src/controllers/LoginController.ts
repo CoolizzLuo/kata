0af4ad4c4c21e79d52474463c02bb490228280a1
Enzo Luo <duenzo1010@gmail.com>
2022-07-13 06:33:03 +0800

    [typescript] Ch259 Advanced Express and TS Integration done

---
+++ typescript/express/src/controllers/LoginController.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import { controller, get, post, use, bodyValidator } from './decorators';
+
+@controller('/auth')
+class LoginController {
+  @get('/login')
+  getLogin(req: Request, res: Response): void {
+    res.send(`
+      <form method="POST">
+        <div>
+          <label>Email</label>
+          <input name="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" />
+        </div>
+        <button>Submit</button>
+      </form>
+    `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response): void {
+    const { email, password } = req.body;
+
+    console.log(email, password);
+
+    if (email && password && email === '132@yahoo.com.tw' && password === '1234') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+    } else {
+      res.send('Invalid email or password');
+    }
+  }
+
+  @get('/logout')
+  getLogout(req: Request, res: Response): void {
+    req.session = undefined;
+    res.redirect('/');
+  }
+}

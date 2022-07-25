aea0689e23bf47e70e972daf8c72d1fc77f1b328
Enzo Luo <duenzo1010@gmail.com>
2022-07-26 02:28:37 +0800

    [react.mrar] ch13 custom router done

--- react/modern_react_with_redux/ch12_widgets/src/App.js
+++ react/modern_react_with_redux/ch12_widgets/src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -35,7 +37,25 @@ const options = [
 ];
 
 const App = () => {
-  return <Translate />;
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div>
+      <Header />
+      <Route path='/'>
+        <Accordion items={items} />
+      </Route>
+      <Route path='/list'>
+        <Search />
+      </Route>
+      <Route path='/dropdown'>
+        <Dropdown label='Select a Language' options={options} selected={selected} onSelectedChange={setSelected} />
+      </Route>
+      <Route path='/translate'>
+        <Translate />
+      </Route>
+    </div>
+  );
 };
 
 export default App;

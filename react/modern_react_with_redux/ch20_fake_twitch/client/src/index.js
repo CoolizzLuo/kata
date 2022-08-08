ec173d634bc8efef3f774d3ab0e20e74bc7b2360
Enzo Luo <duenzo1010@gmail.com>
2022-08-09 06:10:42 +0800

    [react.mrar] ch24 add redux-form

--- react/modern_react_with_redux/ch20_fake_twitch/client/src/index.js
+++ react/modern_react_with_redux/ch20_fake_twitch/client/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './component/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
